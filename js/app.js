7684f5800702393fbbbd12a65bb3aadf137b09e6
Nathan N <Nitharios@users.noreply.github.com>
2017-09-28 12:28:19 -1000

    Able to add items to shopping list!

--- js/app.js
+++ js/app.js
@@ -4,9 +4,15 @@ console.log(sanity);
 
 const content = document.getElementById("content");
 const shoppingList = new ShoppingList();
-
-let renderList = shoppingList.render();
+shoppingList.render();
 
 function add_to_shopping_list() {
+  let nameInput = document.getElementById("name");
+  let descriptionInput = document.getElementById("description");
+  let name = nameInput.value;
+  let description = descriptionInput.value;
   
+  let new_shopping_list_item = new ShoppingListItem(name, description);
+  
+  shoppingList.addItem(new_shopping_list_item);
 }
