f72fb065c83345baf3d993f4842ea4391b497ee4
Nathan N <Nitharios@users.noreply.github.com>
2017-09-28 12:02:05 -1000

    Created app.js file and added scripts to index.html

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// jshint esversion:6
+const sanity = "You're sane!";
+console.log(sanity);
+
+const content = document.getElementById("content");
+const shoppingList = new ShoppingList();
+
+let renderList = shoppingList.render();
